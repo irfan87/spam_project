10d64383c1cf7f6f4fc2d5d82cb14aaf1e6cbcca
Next <nextacademy@email.com>
2016-01-06 18:05:58 +0800

    video embed feat

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20160105153226) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "mercury_images", force: :cascade do |t|
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "post_categories", force: :cascade do |t|
     t.integer  "post_id"
     t.integer  "category_id"
